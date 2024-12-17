d0ee8313f387bf5aacaa385fb9d329de24a7c6fc
Didier Moreno <49493246+mvdidier@users.noreply.github.com>
2024-12-17 12:17:47 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 st.button("Click me")
-st.download_button("Download file", data)
-st.link_button("Go to gallery", url)
+#st.download_button("Download file", data)
+#st.link_button("Go to gallery", url)
 st.page_link("app.py", label="Home")
-st.data_editor("Edit data", data)
+#st.data_editor("Edit data", data)
 st.checkbox("I agree")
 st.feedback("thumbs")
 st.pills("Tags", ["Sports", "Politics"])
